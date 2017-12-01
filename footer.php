957f5e01837485974f09ffeb6259f2d9f150b0da
mqtent <mqtent@gmail.com>
2017-12-01 13:44:25 +0700

    Fix image uri

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
                 <div class="col-sm-4 col-sm-offset-4">	
                     <div class="text-center wow fadeInUp" style="font-size: 14px;">&#9400; TMQ - 2017 </div>
                     <a href="#" class="scrollToTop"><i class="pe-7s-up-arrow pe-va"></i></a>
-                    <a href="tel:0123456789" class="btn-call"><img src="<?php echo get_site_url().'/wp-content/themes/mytheme/imgs/phone.png';?>"></a>
+                    <a href="tel:0123456789" class="btn-call"><img src="<?php echo get_template_directory_uri().'/imgs/phone.png'; ?>"></a>
                 </div>	
             </div>	
         </footer>
