ba799465be363ada6eab83e6a1425e9b936d04a8
mqtent <mqtent@gmail.com>
2017-12-01 13:29:42 +0700

    Update header (title)

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
         <meta charset="<?php bloginfo('charset'); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title><?php echo get_bloginfo( 'name' ); ?></title>
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
         <?php wp_head(); ?>
